ded6a15d82ceb6574593d6ca1a20c174103477e8
Rahul <iamklrahul45@gmail.com>
2025-12-07 22:35:07 +0530

    dghjk

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -33,7 +33,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: true,
+      sameSite: "none",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
@@ -77,7 +77,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: true,
+      sameSite: "none",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
